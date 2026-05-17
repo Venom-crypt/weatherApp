c63d8846f6e7d037efaea1c619a947e374405f86
Venom-crypt <virus34036@gmail.com>
2026-05-18 00:14:12 +0530

    change the project title

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,11 +4,11 @@ const AppHeader = () => {
       <div className="app-logo">🌦️</div>
 
       <div>
-        <h1 className="app-title">ClimaGlass</h1>
+        <h1 className="app-title">Ather</h1>
         <p className="app-subtitle">Live weather, AQI & smart alerts</p>
       </div>
     </header>
   );
-};
+}; 
 
 export default AppHeader;
